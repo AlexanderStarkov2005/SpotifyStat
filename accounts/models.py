e86d55db84ddb5b437080579d473b9b8a4c55405
miroy <miroeg2020@gmail.com>
2026-01-04 15:18:02 +0500

    add user profile model with signals and admin configuration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    spotify_id = models.CharField(
+        max_length=128,
+        unique=True,
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Profile of {self.user.username}'
